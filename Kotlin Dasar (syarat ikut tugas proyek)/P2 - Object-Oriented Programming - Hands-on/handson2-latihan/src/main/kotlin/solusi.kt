ca9d7ce17b9c4033e7f84fa785d88afd67a871a6
Raysitepu <138910848+Raysitepu@users.noreply.github.com>
2026-09-22 08:51:03 +0700

    solusi handson 2 P2

---
+++ Kotlin Dasar (syarat ikut tugas proyek)/P2 - Object-Oriented Programming - Hands-on/handson2-latihan/src/main/kotlin/solusi.kt
@@ -0,0 +1,39 @@
+// Hands-on 2: Interface & Data Class
+
+interface Payable {
+    fun calculateSalary(): Double
+}
+
+data class Employee(
+    val name: String,
+    val baseSalary: Double,
+    val bonus: Double
+) : Payable {
+
+    override fun calculateSalary(): Double {
+        return baseSalary + bonus
+    }
+}
+
+fun main() {
+    val alice = Employee(
+        "Alice",
+        baseSalary = 5_000_000.0,
+        bonus = 500_000.0
+    )
+
+    val bob = alice.copy(name = "Bob")
+
+    println("Gaji ${alice.name}: ${alice.calculateSalary()}")
+    println("Gaji ${bob.name}: ${bob.calculateSalary()}")
+
+    val aliceDuplicate = Employee(
+        "Alice",
+        baseSalary = 5_000_000.0,
+        bonus = 500_000.0
+    )
+
+    println("alice == aliceDuplicate? ${alice == aliceDuplicate}")
+
+    println(alice)
+}
